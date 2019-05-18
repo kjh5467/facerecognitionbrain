288a3ea526f60e76b60e88f1789c432fa6439b3f
unknown <khogan5467@gmail.com>
2019-05-17 20:23:29 -0400

    added clarifai api, added face detection model, added state for input and button submit, and now created a div for image url link.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Rank from "./components/Rank/Rank";
+import Clarifai from "clarifai";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: "6213007769ed4420b43bca2f66390f69"
+});
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: "",
+      imageUrl: ""
+    };
+  }
+
+  onInputChange = e => {
+    this.setState({ input: e.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input.then(
+        function(resp) {
+          console.log(resp.outputs[0].data.regions[0].region_info.bounding_box);
+        },
+        function(err) {}
+      )
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
