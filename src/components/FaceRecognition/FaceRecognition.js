a99d149b42dd1200387494508c6afdae0a3d97ee
unknown <khogan5467@gmail.com>
2019-09-16 13:13:42 -0400

    finished sign in and register pages, still need to solve hash issue with bcrypt

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,25 @@
 import React from "react";
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img src={"imageUrl"} alt="" width="500px" height="auto" />
+        <img
+          id="inputimage"
+          src={"imageUrl"}
+          alt=""
+          width="500px"
+          height="auto"
+        />
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.topRow,
+            bottom: box.bottomRow,
+            left: box.leftCol
+          }}
+        />
       </div>
     </div>
   );
